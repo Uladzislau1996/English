69840d42624345130c8f52caf2dfff7ee96fd24f
Vladislav Galushkevich <39631679+Uladzislau1996@users.noreply.github.com>
2025-12-07 19:32:43 +0200

    Extract bot initialization from bot class

--- src/main/java/telegram/EnglishTeacherBot.java
+++ src/main/java/telegram/EnglishTeacherBot.java
@@ -1,24 +1,15 @@
 package telegram;
 
 import ai.AIClient;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component
 public class EnglishTeacherBot extends TelegramLongPollingBot {
-
-    private static final Logger log = LoggerFactory.getLogger(EnglishTeacherBot.class);
 
     @Value("${telegram.bot-token}")
     private String botToken;
@@ -28,20 +19,6 @@ public class EnglishTeacherBot extends TelegramLongPollingBot {
 
     @Autowired
     private AIClient aiClient;
-
-    @PostConstruct
-    public void registerBot() throws TelegramApiException {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        try {
-            botsApi.registerBot(this);
-        } catch (TelegramApiRequestException e) {
-            if (e.getErrorCode() == 404) {
-                log.warn("Telegram returned 404 while clearing webhook; continuing with long polling", e);
-            } else {
-                throw e;
-            }
-        }
-    }
 
     @Override
     public String getBotToken() {
